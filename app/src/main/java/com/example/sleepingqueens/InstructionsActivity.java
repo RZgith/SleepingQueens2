13260a2d2f63c6ab4af490be7f275d5ac55ff30e
User <roni.za111@gmail.com>
2026-04-26 19:11:29 +0300

    שינוי צבע לכפתור והוראות

---
+++ app/src/main/java/com/example/sleepingqueens/InstructionsActivity.java
@@ -0,0 +1,38 @@
+package com.example.sleepingqueens;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class InstructionsActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_instructions);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        btn=findViewById(R.id.btn);
+        btn.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        Intent i = new Intent(InstructionsActivity.this, MainActivity.class);
+        startActivity(i);
+    }
+}
